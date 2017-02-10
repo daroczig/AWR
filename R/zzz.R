10cc04d793605440bdf17342d98056eeb4f0f432
Gergely Daroczi <gergely.daroczi@card.com>
2017-02-10 16:57:11 +0100

    make sure Java SDK version is in sync with R package version

--- R/zzz.R
+++ R/zzz.R
@@ -10,9 +10,16 @@ NULL
 
 .onLoad <- function(libname, pkgname) {
 
-    ## check if the AWS Java SDJ jars are available and install if needed
+    ## check if the AWS Java SDK jars are available and install if needed
     if (requireNamespace('AWR.jars', quietly = TRUE) == FALSE) {
         warning('AWR.jars package not found, installing now...')
+        install.packages('AWR.jars', repos = 'https://cardcorp.gitlab.io/AWR.jars')
+    }
+
+    ## make sure we have a recent version of the Java SDK
+    if (packageVersion('AWR') > packageVersion('AWR.jars')) {
+        warning(paste('AWR.jars package is older than current AWR package,',
+                      'installing most recent version of the jars now...'))
         install.packages('AWR.jars', repos = 'https://cardcorp.gitlab.io/AWR.jars')
     }
 
