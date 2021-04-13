ec856b5febd6e47925ecc6739242ae4791e5a428
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2021-04-13 10:53:51 +0200

    export fns checking if jars are available

--- R/zzz.R
+++ R/zzz.R
@@ -17,6 +17,14 @@ NULL
 
 .onAttach <- function(libname, pkgname) {
 
+    if (!test_awr_jars()) {
+        packageStartupMessage(check_awr_jars())
+    }
+
+}
+
+.onLoad <- function(libname, pkgname) {
+
     ## add the package-bundled jars to the Java classpath
     rJava::.jpackage(
         pkgname, lib.loc = libname,
@@ -25,20 +33,39 @@ NULL
 
 }
 
-.onLoad <- function(libname, pkgname) {
 
-    ## check if AWS Java SDK was found
+#' Tests if AWS SDK for Java jar files are available
+#' @return boolean
+#' @export
+test_awr_jars <- function() {
     jars <- rJava::.jcall('java/lang/System', 'S', 'getProperty', 'java.class.path')
-    if (!grepl('aws-java-sdk', jars)) {
-        packageStartupMessage(paste(
-            'The AWS Java SDK was not found in the Java JAR class path,',
-            'which means the AWR R package is not ready to be used yet!\n',
-            'If you already have the AWS Java SDK jar files,',
-            'you can use rJava::.jaddClassPath to reference those,',
-            'otherwise you need to compile or download the JAR files',
-            'as described in the README.md of the AWR package.',
-            sep = '\n'
-        ))
-    }
+    grepl('aws-java-sdk', jars)
+}
 
+
+#' Asserts if AWS SDK for Java jar files are available
+#' @return invisible \code{TRUE} on success, otherwise error
+#' @export
+assert_awr_jars <- function() {
+    stopifnot(test_awr_jars())
+    invisible(TRUE)
+}
+
+
+#' Checks if AWS SDK for Java jar files are available
+#' @return \code{TRUE} on success, informative message as a string on error
+#' @export
+check_awr_jars <- function() {
+    if (test_awr_jars()) {
+        return(TRUE)
+    }
+    paste(
+        'The AWS Java SDK was not found in the Java JAR class path,',
+        'which means the AWR R package is not ready to be used yet!\n',
+        'If you already have the AWS Java SDK jar files,',
+        'you can use rJava::.jaddClassPath to reference those,',
+        'otherwise you need to compile or download the JAR files',
+        'as described in the README.md of the AWR package.',
+        sep = '\n'
+    )
 }
